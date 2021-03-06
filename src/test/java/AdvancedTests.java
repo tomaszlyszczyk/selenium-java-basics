2b9cb199f80d0385914ae2943e73ffc5d0595ab9
Tomasz Lyszczyk <tomasz.lyszczyk@predicagroup.com>
2021-03-06 15:19:05 +0100

    asd

--- src/test/java/AdvancedTests.java
+++ src/test/java/AdvancedTests.java
@@ -1,37 +1,41 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebElement;
+import org.junit.jupiter.api.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.support.ui.Select;
 
 // Zmiana testowa
 
 public class AdvancedTests {
 
+    static ChromeDriver driver;
+    ChromeOptions options = new ChromeOptions();
+
+    @BeforeAll
+    static void SetUpAll() {
+        System.out.println("setup all");
+        //         System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+        WebDriverManager.chromedriver().setup();
+    }
+
+    @BeforeEach
+    public void SetUp() {
+        options.addArguments("window-position=2000,0");
+        driver = new ChromeDriver(options);
+        driver.manage().window().maximize();
+    }
+
     @Test
     public void SeleniumBasicTest1() throws InterruptedException {
-//         System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
-        WebDriverManager.chromedriver().setup();
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("window-position=2000,0");
-        ChromeDriver driver = new ChromeDriver(options);
-        driver.manage().window().maximize();
         driver.get("https://aniagotuje.pl/");
-        driver.quit();
     }
 
     @Test
     public void SeleniumBasicTest2() throws InterruptedException {
-//         System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
-        WebDriverManager.chromedriver().setup();
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("window-position=2000,0");
-        ChromeDriver driver = new ChromeDriver(options);
-        driver.manage().window().maximize();
         driver.get("https://www.x-kom.pl/");
-        driver.quit();
+    }
+
+    @AfterEach
+    public void TearDown() {
+        driver.close();
     }
 }
