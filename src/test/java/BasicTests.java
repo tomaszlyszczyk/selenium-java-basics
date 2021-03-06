04bd34506df5d5beef07a18d4ffc59e644981a80
Tomasz Lyszczyk <tomasz.lyszczyk@predicagroup.com>
2021-03-06 10:29:05 +0100

    asd

--- src/test/java/BasicTests.java
+++ src/test/java/BasicTests.java
@@ -1,11 +1,13 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.Test;
 
+// Zmiana testowa
+
 public class BasicTests {
 
     @Test
     public void SeleniumBasicTest() {
-        // System.setProperty("webdriver.chrome.driver", "to_byl_chromedriver.exe");
+         System.setProperty("webdriver.chrome.driver", "to_byl_chromedriver.exe");
         WebDriverManager.chromedriver().setup();
     }
 }
